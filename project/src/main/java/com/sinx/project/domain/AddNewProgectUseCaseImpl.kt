6abc9394ef1871a40f563956cdd1da674e4714b5
SFF <Fail.Salikhov@rosbank.ru>
2023-05-11 23:45:39 +0500

    task- 33, add room in project

--- project/src/main/java/com/sinx/project/domain/AddNewProgectUseCaseImpl.kt
+++ project/src/main/java/com/sinx/project/domain/AddNewProgectUseCaseImpl.kt
@@ -1,12 +1,12 @@
 package com.sinx.project.domain
 
+import com.sinx.project.data.ProjectListModel
+
 internal class AddNewProjectUseCaseImpl(private val projectRepository: ProjectRepository) :
     AddNewProjectUseCase {
-    override suspend fun invoke(newProject: ProjectListModel) {
-        return projectRepository.addNewProject(newProject.toDb())
-    }
+    override suspend fun invoke(newProject: ProjectListModel) = projectRepository.addNewProject(newProject)
 }
 
 internal interface AddNewProjectUseCase {
-    suspend operator fun invoke(newProject: ProjectListModel)
+    operator fun invoke(newProject: ProjectListModel)
 }
