782127b78178ac775a9692eb94f9797e86514eb1
SFF <Fail.Salikhov@rosbank.ru>
2023-05-29 22:02:22 +0500

    task-67, creat coreDbImpl module

--- coreDB/coreDbImpl/build.gradle.kts
+++ coreDB/coreDbImpl/build.gradle.kts
@@ -12,14 +12,8 @@ dependencies {
 
     implementation(Dependency.Android.core)
     implementation(Dependency.Compat.appCompat)
-    implementation(project(Config.Modules.core))
-    implementation(project(Config.Modules.coreDbApi))
-    implementation(project(Config.Modules.coreDbImpl))
 
     implementation(Dependency.Room.runtime)
     kapt(Dependency.Room.compiler)
     implementation(Dependency.Room.ktx)
-
-    implementation(Dependency.DaggerDI.dagger)
-    kapt(Dependency.DaggerDI.daggerCompiler)
 }
