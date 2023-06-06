2c6cae9e2ebdf9976021e39fe034d51ce69fe75a
SFF <Fail.Salikhov@rosbank.ru>
2023-06-06 20:54:23 +0500

    task-68 ребейз

--- coreDB/coreDbImpl/build.gradle.kts
+++ coreDB/coreDbImpl/build.gradle.kts
@@ -13,8 +13,13 @@ dependencies {
     implementation(Dependency.Android.core)
     implementation(Dependency.Compat.appCompat)
 
+    implementation(project(Config.Modules.core))
+
     implementation(Dependency.Room.runtime)
     implementation(project(mapOf("path" to ":coreDB:coreDbApi")))
     kapt(Dependency.Room.compiler)
     implementation(Dependency.Room.ktx)
+
+    implementation(Dependency.DaggerDI.dagger)
+    kapt(Dependency.DaggerDI.daggerCompiler)
 }
