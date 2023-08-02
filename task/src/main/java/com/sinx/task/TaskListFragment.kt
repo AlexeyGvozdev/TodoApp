28d8490ac0def24c231baf69098804feb5d6ca97
vladi <vladislavac74@gmail.com>
2023-08-02 06:56:35 +0300

    Правки

--- task/src/main/java/com/sinx/task/TaskListFragment.kt
+++ task/src/main/java/com/sinx/task/TaskListFragment.kt
@@ -74,7 +74,7 @@ class TaskListFragment : Fragment(R.layout.task_list_layout) {
 
             override fun onTaskTitleClickListener(task: TaskItem) {
                         val request = NavDeepLinkRequest.Builder.fromUri(
-           "$INNER_TASK_URI?$TASK_BUNDLE_KEY=${task.name}&$TASK_DATE_BUNDLE_KEY=${task.date}".toUri()
+            "$INNER_TASK_URI?$TASK_BUNDLE_KEY=${task.name}&$TASK_DATE_BUNDLE_KEY=${task.date}".toUri()
                     )
                     .build()
                 findNavController().navigate(request)
