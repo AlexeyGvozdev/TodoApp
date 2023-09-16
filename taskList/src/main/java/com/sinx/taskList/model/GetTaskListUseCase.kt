1edd0b17d06f1ccb095492cd06e4fa3ba00e7ab1
vladi <vladislavac74@gmail.com>
2023-09-16 18:36:31 +0300

    Правки по дизайну

--- taskList/src/main/java/com/sinx/taskList/model/GetTaskListUseCase.kt
+++ taskList/src/main/java/com/sinx/taskList/model/GetTaskListUseCase.kt
@@ -2,10 +2,9 @@ package com.sinx.taskList.model
 
 import com.sinx.taskList.TaskItem
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class GetTaskListUseCaseImpl(private val repository: TaskRepository) : GetTaskListUseCase {
-    override suspend operator fun invoke()=repository.listTasksFlow()
+    override suspend operator fun invoke() = repository.listTasksFlow()
 }
 
 interface GetTaskListUseCase {
